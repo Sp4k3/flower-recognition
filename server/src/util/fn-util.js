3ea995a746c661e36748e16fbfc1802146bc65f3
Thibault COLIN <thibault_colin@hotmail.fr>
2021-02-05 15:58:40 +0100

    :bug: Debug model training

--- server/src/util/fn-util.js
+++ server/src/util/fn-util.js
@@ -6,18 +6,16 @@ export const shuffle = (arr) => arr.sort(() => Math.random() - 0.5)
 
 // Browse all images path from a given image folder
 export const walk = async (dir) => {
-  console.log(dir)
-  let files = await fs.readdir(dir)
+  let files = await fs.promises.readdir(dir)
   files = await Promise.all(
     files.map(async file => {
       const filePath = path.join(dir, file)
-      const stats = fs.stat(filePath)
+      const stats = await fs.promises.stat(filePath)
       if (stats.isDirectory()) {
         return walk(filePath)
       } else if (stats.isFile()) {
         return filePath
       }
     }))
-  console.log('files : ', await files)
   return files.reduce((all, folderContents) => all.concat(folderContents), [])
 }
